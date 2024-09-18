28f4b5ed0f6afb2594efae2468f1e359e497af29
AakritiPandit <aakritipandit777@gmail.com>
2024-09-18 08:59:00 +0545

    aakritichanged

---
+++ javascript/script.js
@@ -0,0 +1,6 @@
+
+document.querySelector('footer button').addEventListener('click', function() {
+    alert('Redirecting to registration page...');
+    // You can add redirection code here.
+    window.location.href = 'register.html'; // Example redirection
+});
